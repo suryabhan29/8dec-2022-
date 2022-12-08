67a47735b4b8938048151a25dfd1df7fe51b3a97
suryabhan29 <suryabhansingh1029@gmail.com>
2022-12-08 16:22:08 +0530

    java script learning commit 1

---
+++ js/script2.js
@@ -0,0 +1,13 @@
+
+
+
+const _students = ["yogi",'vikas',`arpit`,"rakesh"]; // this is a java script array
+
+// arrayname [indexnumber]
+
+console.log('hello '+_students[0]);
+console.log('hello '+_students[1]+` my brother`);
+console.log('hello '+_students[2]);
+console.log('hello '+_students[3]);
+
+// here is not run is brower here check out output is terminal (external)
